fea49c51518bc2691c69b92a7cff296e50adf912
Kerkey <dunkeroket@gmail.com>
2022-09-19 11:58:24 +0300

    otchet added

--- app/src/main/java/com/example/myfirstapplication/HelloActivity.java
+++ app/src/main/java/com/example/myfirstapplication/HelloActivity.java
@@ -4,35 +4,49 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 
 public class HelloActivity extends Activity {
+    int count1 = 0;
+    int count2 = 0;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_helloact);
 
-        Button button1 = findViewById(R.id.button1);
-        button1.setOnClickListener(new View.OnClickListener() {
-            int count = 0;
 
+        Button button1 = findViewById(R.id.button1);
+        TextView tv1 = findViewById(R.id.tv1);
+        button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                count++;
-                button1.setText(String.valueOf(count));
+                count1++;
+                tv1.setText(String.valueOf(count1));
             }
         });
 
 
-        Button buttonClickCount = findViewById(R.id.buttonClickCount);
-        buttonClickCount.setOnClickListener(new View.OnClickListener() {
-            int count = 0;
-
+        Button button2 = findViewById(R.id.button2);
+        TextView tv2 = findViewById(R.id.tv2);
+        button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                count++;
-                buttonClickCount.setText(String.valueOf(count));
+                count2++;
+                tv2.setText(String.valueOf(count2));
+            }
+        });
+
+        Button resetButton = findViewById(R.id.buttonReset);
+        resetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                count1 = 0;
+                count2 = 0;
+                tv1.setText(String.valueOf(count1));
+                tv2.setText(String.valueOf(count2));
             }
         });
     }
